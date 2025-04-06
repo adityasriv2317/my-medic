e55e0fe112c6628125f8b3ade4fd8017f9d7d435
adityasriv2317 <adityaxia9237@gmail.com>
2025-04-07 02:42:09 +0530

    location fix

--- src/pages/Medicines.jsx
+++ src/pages/Medicines.jsx
@@ -8,7 +8,7 @@ import Footer from '../components/Footer';
 import { toast } from 'react-toastify';
 import ToastBox from '../components/ToastBox';
 
-import medicine from '../../public/medApi';
+import medicine from '../Data/medApi';
 
 const Medicines = () => {
   const { user, loading } = useContext(WebContext);
