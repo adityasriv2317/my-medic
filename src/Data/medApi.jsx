45ace287d542d552d730b58bfc5d4bd7d3837421
adityasriv2317 <adityaxia9237@gmail.com>
2025-04-07 02:54:40 +0530

    a

--- src/Data/medApi.jsx
+++ src/Data/medApi.jsx
@@ -1,15 +1,15 @@
-import aspirin from "../medicines/aspirin.jpg";
-import cetirizine from "../medicines/cetirizine.jpg";
-import calciumD3 from "../medicines/calciumD3.jpg";
-import firstAid from "../medicines/firstAid.jpg";
-import paracetamol from "../medicines/paracetamol.jpg";
-import vitaminD3 from "../medicines/vitaminD3.jpg";
-import amoxycillin from "../medicines/amoxycillin.jpg";
-import azythromycin from "../medicines/azythromycin.jpg";
-import multivitamin from "../medicines/multivitamin.jpg";
-import ibuprofen from "../medicines/ibuprofen.jpg";
-import bandage from "../medicines/bandage.jpg";
-import antiseptic from "../medicines/antiseptic.jpg";
+import aspirin from "/medicines/aspirin.jpg";
+import cetirizine from "/medicines/cetirizine.jpg";
+import calciumD3 from "/medicines/calciumD3.jpg";
+import firstAid from "/medicines/firstAid.jpg";
+import paracetamol from "/medicines/paracetamol.jpg";
+import vitaminD3 from "/medicines/vitaminD3.jpg";
+import amoxycillin from "/medicines/amoxycillin.jpg";
+import azythromycin from "/medicines/azythromycin.jpg";
+import multivitamin from "/medicines/multivitamin.jpg";
+import ibuprofen from "/medicines/ibuprofen.jpg";
+import bandage from "/medicines/bandage.jpg";
+import antiseptic from "/medicines/antiseptic.jpg";
 
 const medicine = [
     paracetamol,
@@ -26,5 +26,4 @@ const medicine = [
     cetirizine,
 ];
 
-// This is a list of medicines with their respective images imported from the medicines folder.
 export default medicine;
